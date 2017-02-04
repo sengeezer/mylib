a92335900b11879122be8e9a0300a6444f587dbd
Fabio Albertin <albertin@gmail.com>
2017-02-04 23:01:12 +0000

    Added actual tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,26 @@ let expect = require('chai').expect;
 let starwars = require('./index');
 
 describe('my-lib-a', function() {
-  it('should work', function() {
-    expect(true).to.be.true;
+  describe('all', function() {
+    it('should be an array of strings', function() {
+      expect(starwars.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === 'string';
+        });
+      }
+    });
+
+    it('should contain `Luke Skywalker`', function() {
+      expect(starwars.all).to.include('Luke Skywalker');
+    });
+  });
+
+  describe('random', function() {
+    it('should return a random item from starwars.all', function() {
+      let randomItem = starwars.random();
+      expect(starwars.all).to.include(randomItem);
+    });
   });
 });
