a97e1d35ba9d0dad482707fc34c6e700b84cf288
Fabio Albertin <albertin@gmail.com>
2017-02-04 22:13:06 +0000

    Adde mocha and chai for testing

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+let expect = require('chai').expect;
+let starwars = require('./index');
+
+describe('my-lib-a', function() {
+  it('should work', function() {
+    expect(true).to.be.true;
+  });
+});
